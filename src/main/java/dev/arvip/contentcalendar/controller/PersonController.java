907b9a81e426473766c77a7a0d4bb9304f6994cb
arvipDev <arvind.purushotham@gmail.com>
2023-12-31 19:11:38 -0500

    CRUD for the person impl

--- src/main/java/dev/arvip/contentcalendar/controller/PersonController.java
+++ src/main/java/dev/arvip/contentcalendar/controller/PersonController.java
@@ -1,13 +1,13 @@
 package dev.arvip.contentcalendar.controller;
 
 import dev.arvip.contentcalendar.model.Person;
+import dev.arvip.contentcalendar.model.Sex;
 import dev.arvip.contentcalendar.repository.ContentRepository;
 import dev.arvip.contentcalendar.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -27,5 +27,47 @@ public class PersonController {
         return repository.findAll();
     }
 
+    @GetMapping("/{id}")
+    public Person getById(@PathVariable Integer id){
+        return repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    public void create(@RequestBody Person person){
+        System.out.println(person);
+        if (!repository.findById(person.personId()).isEmpty())  throw new ResponseStatusException(HttpStatus.OK);
+        Person per = new Person(person.firstName(), person.lastName(), person.age(), person.sex(),
+                person.phoneNumber(), person.address(), null);
+        repository.save(per);
+
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @PutMapping("/{id}")
+    public void update(@RequestBody Person person, @PathVariable Integer id){
+        if(repository.findById(person.personId()).isEmpty()) {
+            repository.save(new Person(person.firstName(), person.lastName(), person.age(), person.sex(),
+                    person.phoneNumber(), person.address(), null));
+            throw new ResponseStatusException(HttpStatus.CREATED);
+        }
+        repository.save(person);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Integer id){
+        repository.deleteById(id);
+    }
+
+    @GetMapping("/filter/age/{age}")
+    public List<Person> getAllByAge(@PathVariable Integer age){
+        return repository.getAllByAge(age);
+    }
+
+    @GetMapping("/filter/sex/{sex}")
+    public Person getBySex(@PathVariable String sex){
+        return repository.getBySex(Sex.valueOf(sex.toUpperCase()));
+    }
 
 }
--- src/main/java/dev/arvip/contentcalendar/repository/PersonRepository.java
+++ src/main/java/dev/arvip/contentcalendar/repository/PersonRepository.java
@@ -1,8 +1,16 @@
 package dev.arvip.contentcalendar.repository;
 
 import dev.arvip.contentcalendar.model.Person;
+import dev.arvip.contentcalendar.model.Sex;
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.util.List;
+
 public interface PersonRepository extends ListCrudRepository<Person, Integer> {
+
+    List<Person> getAllByAge(Integer age);
+
+
+    Person getBySex(Sex sex);
 
 }
