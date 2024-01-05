890210264de624f3733efc207b08cb45244409f8
arvipDev <arvind.purushotham@gmail.com>
2024-01-05 00:11:17 -0500

    test

--- src/main/java/dev/arvip/contentcalendar/repository/PersonRepository.java
+++ src/main/java/dev/arvip/contentcalendar/repository/PersonRepository.java
@@ -3,9 +3,11 @@ package dev.arvip.contentcalendar.repository;
 import dev.arvip.contentcalendar.model.Person;
 import dev.arvip.contentcalendar.model.Sex;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PersonRepository extends ListCrudRepository<Person, Integer> {
 
     List<Person> getAllByAge(Integer age);
