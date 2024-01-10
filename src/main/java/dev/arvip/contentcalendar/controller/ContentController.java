27123bea8ab647578f372f33ed00f607b746c9e9
arvipDev <arvind.purushotham@gmail.com>
2024-01-09 22:22:31 -0500

    Added session impl with max time interval between visits

--- src/main/java/dev/arvip/contentcalendar/controller/ContentController.java
+++ src/main/java/dev/arvip/contentcalendar/controller/ContentController.java
@@ -3,10 +3,12 @@ package dev.arvip.contentcalendar.controller;
 import dev.arvip.contentcalendar.model.Content;
 import dev.arvip.contentcalendar.model.Status;
 import dev.arvip.contentcalendar.repository.ContentRepository;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.security.Principal;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,24 +17,41 @@ import java.util.Optional;
 @RequestMapping("/api/content")
 public class ContentController {
     private final ContentRepository repository;
+    private String COUNTER = "COUNTER";
 
     public ContentController(ContentRepository repository){
         this.repository = repository;
     }
 
+    @GetMapping("/counter")
+    public String  getPageVisitsInSession(Principal Principal, HttpSession httpSession){
+        return "Visits " + incrementCounter(httpSession, COUNTER);
+    }
+
+    //counter for session.
+    public String incrementCounter(HttpSession httpSession, String attr){
+        var session = httpSession.getAttribute(attr) == null ? 0 : (Integer) httpSession.getAttribute(attr);
+        httpSession.setAttribute(COUNTER, ++session);
+        httpSession.setMaxInactiveInterval(120);
+        return "to the site: " + httpSession.getAttribute(attr) ;
+    }
+
     @GetMapping("")
-    public List<Content> getAll(){
+    public List<Content> getAll(HttpSession httpSession){
+        incrementCounter(httpSession, COUNTER);
         return repository.findAll();
     }
 
     @GetMapping("/{id}")
-    public Content getById(@PathVariable Integer id){
+    public Content getById(@PathVariable Integer id, HttpSession httpSession){
+        incrementCounter(httpSession, COUNTER);
         return repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("")
-    public void create(@RequestBody Content content){
+    public void create(@RequestBody Content content, HttpSession httpSession){
+        incrementCounter(httpSession, COUNTER);
         if (!repository.findById(content.id()).isEmpty()) throw new ResponseStatusException(HttpStatus.OK);
         Content cnt = new Content(content.dateCreated(), content.dateUpdated(), null,
                 content.desc(), content.title(), content.status(), content.contentType(), content.url());
@@ -41,23 +60,27 @@ public class ContentController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PutMapping("/{id}")
-    public void update(@RequestBody Content content, @PathVariable Integer id){
+    public void update(@RequestBody Content content, @PathVariable Integer id, HttpSession httpSession){
+        incrementCounter(httpSession, COUNTER);
         repository.save(content);
     }
 
     @DeleteMapping("/{id}")
-    public void remove(@PathVariable Integer id){
+    public void remove(@PathVariable Integer id, HttpSession httpSession){
+        incrementCounter(httpSession, COUNTER);
         repository.deleteById(id);
     }
 
 
     @GetMapping("/filter/title/{title}")
-    public List<Content> findAllByTitle(@PathVariable String title){
+    public List<Content> findAllByTitle(@PathVariable String title, HttpSession httpSession){
+        incrementCounter(httpSession, COUNTER);
         return repository.findAllByTitleContains(title);
     }
 
     @GetMapping("/filter/status/{status}")
-    public List<Content> findAllWithStatus(@PathVariable Status status){
+    public List<Content> findAllWithStatus(@PathVariable Status status, HttpSession httpSession){
+        incrementCounter(httpSession, COUNTER);
         return repository.findAllByStatusEquals(status);
     }
 
