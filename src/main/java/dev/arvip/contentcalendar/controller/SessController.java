9e141ca3949f78d222918b111314e586b98a7460
arvipDev <arvind.purushotham@gmail.com>
2024-01-09 21:45:16 -0500

    session persisting data, set max time between activities

--- src/main/java/dev/arvip/contentcalendar/controller/SessController.java
+++ src/main/java/dev/arvip/contentcalendar/controller/SessController.java
@@ -1,5 +1,6 @@
 package dev.arvip.contentcalendar.controller;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +11,29 @@ import java.security.Principal;
 //@RequestMapping("/sess")
 public class SessController {
 
+    private String SESS_COUNT = "SESS_COUNT";
+
     @GetMapping("/")
-    public String home(Principal principal){
+    public String home(Principal principal, HttpSession httpSession){
+        System.out.println(principal.toString());
+        incrementCounter(httpSession, SESS_COUNT);
         return "Hello " + principal.getName();
+    }
+
+    @GetMapping("/count")
+    public String getCounter(HttpSession httpSession){
+        return incrementCounter(httpSession, SESS_COUNT);
+    }
+
+    public String incrementCounter(HttpSession httpSession, String attr){
+        System.out.println(httpSession.getAttributeNames());
+        System.out.println("Creating time: " + httpSession.getCreationTime());
+        System.out.println("Last accessed time: " + httpSession.getLastAccessedTime());
+        System.out.println("ID " + httpSession.getId());
+
+        var session = httpSession.getAttribute(attr) == null ? 0 : (Integer) httpSession.getAttribute(attr);
+        httpSession.setAttribute(attr, ++session);
+        httpSession.setMaxInactiveInterval(120);
+        return "SESS_COUNT: " + httpSession.getAttribute(attr);
     }
 }
